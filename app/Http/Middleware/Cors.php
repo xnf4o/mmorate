254ab375e325e1a983a7c0ca2ee0aa65b67658cf
janny <eugerasimoff@gmail.com>
2018-10-06 11:32:58 +0300

    Fix

---
+++ app/Http/Middleware/Cors.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class Cors
+{
+    public function handle($request, Closure $next)
+    {
+        header('Access-Control-Allow-Origin: *');
+        header('Content-Type: application/json');
+
+        $headers = [
+            'Access-Control-Allow-Methods' => 'POST, GET, OPTIONS, PUT, DELETE',
+            'Access-Control-Allow-Headers' => 'X-Requested-With, Content-Type, X-Auth-Token, Origin, Authorization, Application, Accept'
+        ];
+        if ($request->getMethod() == "OPTIONS") {
+            return Response::make('OK', 200, $headers);
+        }
+
+        $response = $next($request);
+        foreach ($headers as $key => $value)
+            $response->header($key, $value);
+        return $response;
+    }
+}
